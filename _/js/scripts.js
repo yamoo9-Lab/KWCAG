bddb9962cdfabe2dee2be525fa555c0d444e3b18
Jee Hoon <yamoo9@naver.com>
2015-03-18 17:51:26 +0900

    2일차 수업 진행내용 반영

--- _/js/scripts.js
+++ _/js/scripts.js
@@ -1,4 +1,5 @@
-(function(){
+//
+(function(global){
 
 	// BGM
 	var BGM = new Audio(),
@@ -6,6 +7,8 @@
 
 	// BGM 곡 설정
 	BGM.src = '../_/media/We The Kings -Say You Like Me.' + ext;
+
+	// console.log(BGM.src);
 
 	// BGM 자동 재생
 	BGM.oncanplay = function() { this.play() };
@@ -15,8 +18,23 @@
 		BGM.pause();
 	}
 
+	// global.stopBGM = stopBGM;
+
 	// ESC(27) 키 누르면 배경음악 정지
+	// 사용자가 ESC 키를 누르면,
+	// 키코드 값을 감지하여 ESC에 해당되는 키 값과 비교하여 일치하면
+	// 함수 stopBGM()을 실행하라
+	document.onkeydown = function(e) {
+		if (e.keyCode === 27) {
+			stopBGM();
+		}
+	};
+
 
 	// .mute-BGM 버튼 누르면 stopBGM 함수 실행
+	var muteBtn = document.querySelector('.mute-BGM');
 
-})();
+	muteBtn.onclick = stopBGM;
+
+
+})(window);
