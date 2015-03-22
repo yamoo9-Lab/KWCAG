006af4d1ef9fff14daaabafa64a0453024b49dba
Jee Hoon <yamoo9@naver.com>
2015-03-22 13:18:45 +0900

    인식의 용이성 예제 (접근성 코드 적용) 추가

--- _/js/scripts-accessible.js
+++ _/js/scripts-accessible.js
@@ -24,7 +24,19 @@
 	// 사용자가 ESC 키를 누르면,
 	// 키코드 값을 감지하여 ESC에 해당되는 키 값과 비교하여 일치하면
 	// 함수 stopBGM()을 실행하라
+	document.onkeydown = function(e) {
+		if (e.keyCode === 27) {
+			stopBGM();
+		}
+	};
+
 
 	// .mute-BGM 버튼 누르면 stopBGM 함수 실행
+	var muteBtn = document.querySelector('.mute-BGM');
+	muteBtn.onclick = function(e){
+		stopBGM();
+		return false;
+	};
+
 
 })(window);
