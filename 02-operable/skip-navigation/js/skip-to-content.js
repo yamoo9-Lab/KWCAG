dad32bb279f10c0571b740e97b024d011d93e249
Jee Hoon <yamoo9@naver.com>
2015-03-25 09:55:29 +0900

    숙제 자료 추가

--- 02-operable/skip-navigation/js/skip-to-content.js
+++ 02-operable/skip-navigation/js/skip-to-content.js
@@ -1,15 +1,15 @@
 if (!('bind' in Function.prototype)) {
-    Function.prototype.bind= function(owner) {
-        var that= this;
-        if (arguments.length<=1) {
-            return function() { return that.apply(owner, arguments); };
-        } else {
-            var args= Array.prototype.slice.call(arguments, 1);
-            return function() {
-                return that.apply(owner, arguments.length===0? args : args.concat(Array.prototype.slice.call(arguments)));
-            };
-        }
-    };
+	Function.prototype.bind= function(owner) {
+		var that= this;
+		if (arguments.length<=1) {
+			return function() { return that.apply(owner, arguments); };
+		} else {
+			var args= Array.prototype.slice.call(arguments, 1);
+			return function() {
+				return that.apply(owner, arguments.length===0? args : args.concat(Array.prototype.slice.call(arguments)));
+			};
+		}
+	};
 };
 
 /**
